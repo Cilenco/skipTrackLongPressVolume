2241a9b14e2a67fe3913bc8926354a6113e61c12
Nikhil Paranjape <indianpoptart@users.noreply.github.com>
2019-04-12 20:51:09 -0400

    Root checker
    - This adds a root checker to the intro permission check
    - NOT TESTED, this may or may not work

--- app/src/main/java/com/cilenco/skiptrack/ui/activities/MainActivity.kt
+++ app/src/main/java/com/cilenco/skiptrack/ui/activities/MainActivity.kt
@@ -6,11 +6,15 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 import com.cilenco.skiptrack.R
 import com.cilenco.skiptrack.utils.Constants.Companion.PREF_PERMISSION
 
 import net.grandcentrix.tray.AppPreferences
+import eu.chainfire.libsuperuser.Shell
+
+
 
 
 class MainActivity : AppCompatActivity(), DialogInterface.OnClickListener {
@@ -32,20 +36,26 @@ class MainActivity : AppCompatActivity(), DialogInterface.OnClickListener {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            val prefs = AppPreferences(this)
-            prefs.put(PREF_PERMISSION, true)
+        if(Shell.SU.available()){
+            val shell = Shell.Pool.SU.get()
+            shell.run("shell pm grant com.cilenco.skiptrack android.permission.SET_VOLUME_KEY_LONG_PRESS_LISTENER")
         } else {
-            val builder = AlertDialog.Builder(this)
+            Log.d("cilenco", "No Root :(")
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val prefs = AppPreferences(this)
+                prefs.put(PREF_PERMISSION, true)
+            } else {
+                val builder = AlertDialog.Builder(this)
 
-            builder.setPositiveButton(android.R.string.ok, this)
+                builder.setPositiveButton(android.R.string.ok, this)
 
-            builder.setView(R.layout.dialog_permission)
-            builder.setMessage(R.string.permission_description)
-            builder.setTitle(R.string.permission_title)
-            builder.setCancelable(false)
+                builder.setView(R.layout.dialog_permission)
+                builder.setMessage(R.string.permission_description)
+                builder.setTitle(R.string.permission_title)
+                builder.setCancelable(false)
 
-            builder.show()
+                builder.show()
+            }
         }
     }
 
